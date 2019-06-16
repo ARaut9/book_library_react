aaf30677cd4d8355041dbcf766b9d37dd6cc2dec
ARaut9 <ankitraut999@gmail.com>
2019-06-16 22:17:43 +0530

    comlete book table rendering feature

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -4,12 +4,24 @@ class BookItem extends React.Component {
     render() {
         return (
             <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td>{this.props.book.id}</td>
+                <td>{this.props.book.title}</td>
+                <td>{this.props.book.author}</td>
+                <td>{this.props.book.pages}</td>
+                <td>
+                    <button onClick={() => this.props.toggleReadStatus(this.props.book.id)}>
+                        {
+                            this.props.book.readStatus ? 
+                            <img src="/icons/toggle-on.png" alt="read"/> : 
+                            <img src="/icons/toggle-off.png" alt="not read"/>
+                        }
+                    </button>
+                </td>
+                <td>
+                    <button onClick={() => this.props.deleteBookItem(this.props.book.id)}>
+                        <img src="/icons/delete.png" alt="delete"/>
+                    </button>
+                </td>
             </tr>
         )
     }
--- src/components/Table.js
+++ src/components/Table.js
@@ -3,22 +3,6 @@ import TableHead from './TableHead';
 import TableBody from './TableBody';
 
 class Table extends React.Component {
-    state = {
-        books: [
-            {
-              title: "The Hobbit",
-              author: "J.R.R. Tolkien",
-              pages: 295,
-              readStatus: false,
-            },
-            {
-              title: "Harry Potter",
-              author: "J.K. Rowling",
-              pages: 150,
-              readStatus: false,
-            },
-        ]
-    }
     render() {
         return (
             <div className="table-container">
